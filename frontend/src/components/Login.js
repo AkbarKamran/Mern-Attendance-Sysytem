798c0e15c201a0a9db6727bc8608ad5df542164a
AkbarKamran <akbarkamran121@gmail.com>
2021-09-19 00:13:30 +0500

    first commit

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+import Axios from "axios";
+
+import "../App.css";
+
+export default class Login extends React.Component {
+  constructor() {
+    super();
+    let loggedIn = false;
+
+    const token = localStorage.getItem("token");
+    if (token) loggedIn = true;
+
+    this.state = {
+      username: "",
+      password: "",
+      loggedIn,
+      error: "",
+    };
+    this.onChange = this.onChange.bind(this);
+    this.formSubmit = this.formSubmit.bind(this);
+  }
+
+  onChange(ev) {
+    this.setState({
+      [ev.target.name]: ev.target.value,
+    });
+  }
+
+  async formSubmit(ev) {
+    ev.preventDefault();
+    const { username, password } = this.state;
+    try {
+      const token = await Axios.post("/login", { username, password });
+      localStorage.setItem("token", token);
+      this.setState({
+        loggedIn: true,
+      });
+    } catch (err) {
+      window.location.reload();
+      this.setState({
+        error: err.message,
+      });
+    }
+  }
+
+  render() {
+    if (this.state.loggedIn === true) {
+      return <Redirect to="/user" />;
+    }
+
+    return (
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="card">
+              <form onSubmit={this.formSubmit} class="box">
+                <h1>NeooPal Login</h1>
+                <p class="text-muted">Please enter your login and password!</p>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  value={this.state.username}
+                  onChange={this.onChange}
+                  name="username"
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  name="password"
+                />
+                <a class="forgot text-muted" href="/#">
+                  Forgot password?
+                </a>
+                <input type="submit" value="Login" />
+                {this.state.error}
+              </form>
+              <Redirect to="/" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ frontend/src/components/User.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+export default class User extends React.Component {
+  constructor() {
+    super();
+    let loggedIn = false;
+
+    const token = localStorage.getItem("token");
+    if (token) loggedIn = true;
+    this.logout = this.logout.bind(this);
+    this.state = {
+      loggedIn,
+    };
+  }
+
+  logout() {
+    this.setState({
+      loggedIn: false,
+    });
+  }
+
+  render() {
+    if (this.state.loggedIn === false) {
+      return <Redirect to="/logout" />;
+    }
+    return (
+      <>
+        <div>
+          <div class="wrapper ">
+            <div
+              class="sidebar"
+              data-color="purple"
+              data-background-color="black"
+              data-image="../assets/img/sidebar-2.jpg"
+            >
+              <br />
+              <br />
+              <br />
+
+              <div class="logo">
+                <a
+                  href="http://www.creative-tim.com"
+                  class="simple-text logo-normal"
+                >
+                  Creative Tim
+                </a>
+              </div>
+              <div class="sidebar-wrapper">
+                <ul class="nav">
+                  <li class="nav-item   ">
+                    <a class="nav-link" href="./dashboard.html">
+                      <p>Dashboard</p>
+                    </a>
+                  </li>
+                  <li class="nav-item ">
+                    <a class="nav-link" href="./user.html">
+                      <p>Report</p>
+                    </a>
+                  </li>
+                  <li class="nav-item ">
+                    <a class="nav-link" href="./tables.html">
+                      <p>Request Leave Form</p>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container1">
+          <button class="btn btn-primary btn-lg" onClick={this.logout}>
+            Logout
+          </button>
+        </div>
+      </>
+    );
+  }
+}
